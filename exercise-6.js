b30423c2d26b5f8c0f1f854564c583d1db1fa9af
Renault Dzaky <40533329+reduksi@users.noreply.github.com>
2019-09-29 20:18:21 +0700

    yagitu

--- exercise-6.js
+++ exercise-6.js
@@ -39,6 +39,8 @@ console.log('LOOPING KEDUA');
 for(i = 1; i <= 100; i+=2){
     if(i%3 === 0){
         console.log(i + 'kelipatan 3');
+    }else{
+        console.log('');
     }
 }
 
@@ -46,6 +48,8 @@ console.log('LOOPING KETIGA');
 for(i = 1; i <= 100; i+=5){
     if(i%6 === 0){
         console.log(i + 'kelipatan 6');
+    }else{
+        console.log('');
     }
 }
 
@@ -53,5 +57,7 @@ console.log('LOOPING KEEMPAT');
 for(i = 1; i <= 100; i+=9){
     if(i%10 === 0){
         console.log(i + 'kelipatan 10');
+    }else{
+        console.log('');
     }
 }
